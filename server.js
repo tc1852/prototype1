95f287084e1905d24882d61c8f6e3972eb7f4267
Tony Choi <tonychoi.cs@gmail.com>
2019-05-30 18:57:55 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World! - Prototype1'));
+app.listen(port, () => console.log(`app running on port ${port}`));
