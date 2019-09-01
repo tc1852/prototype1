cdaba048293e6962c7c679e84dbb4a94938e20e7
Tony Choi <tchoi@fandango.com>
2019-08-31 21:56:18 -0700

    Added readme

---
+++ README.txt
@@ -0,0 +1,7 @@
+How to run: 
+[From terminal]
+run: node server
+run w/ nodemon: nodemon server
+
+[from browser]
+localhost:3000
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World! - Prototype1'));
+app.get('/', (req, res) => res.send('Hello World! - Prototype3'));
 app.listen(port, () => console.log(`app running on port ${port}`));
