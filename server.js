9ab5d02786e1884e259c3b7c832593c3fcabb425
github <tonychoi.cs@gmail.com>
2019-10-10 18:52:46 -0700

    updated text

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World! - Prototype4'));
+app.get('/', (req, res) => res.send('Hello World! - Prototype5'));
 app.listen(port, () => console.log(`app running on port ${port}`));
